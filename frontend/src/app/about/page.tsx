dc75193edc55ab0bde66d2297d08faed0d8d5773
Sureshkumar Selvaraj <suresh.selvaraj@onedigital.com>
2025-09-04 17:18:43 +0530

    about sample page

---
+++ frontend/src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header";
+
+export default function AboutPage() {
+    return (
+        <main className="min-h-screen">
+            <Header/>
+            <div className="bg-gray-50 py-12">
+                About Page
+            </div>
+        </main>
+    )
+}
